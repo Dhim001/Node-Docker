4283888699c4648b5f63c68f9dad93b30481fe97
Dimeji Rufai <rfdimeji@gmail.com>
2024-01-04 23:38:43 +0000

    created the model data schema, controller, & route functionality

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const {
   MONGO_PORT,
 } = require("./config/config");
 
+const postRouter = require("./routes/postRoutes")
+
 const app = express();
 
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
@@ -26,11 +28,15 @@ const connectWithRetry = () => {
 
 connectWithRetry();
 
+// Defining a middleware
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("<h2>Hi There</h2>");
 });
 
+//localhost:3000/api/v1/post/
+app.use("/api/v1/posts", postRouter)
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`listening on port ${port}`));
