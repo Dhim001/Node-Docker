62fa9a0d971c10cdfbc076343d80f1aa25a57ae4
Dimeji Rufai <rfdimeji@gmail.com>
2024-01-04 22:50:10 +0000

    did some major config to the docker-composefile.

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_IP,
+  MONGO_PORT,
+} = require("./config/config");
 
 const app = express();
 
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+
+const connectWithRetry = () => {
+  mongoose
+  .connect(
+   mongoURL
+  )
+  .then(() => console.log("successfully connected to DB"))
+  .catch((e) => {
+    console.log(e);
+    setTimeout(connectWithRetry, 5000);
+  
+  });
+}
+
+connectWithRetry();
+
+
 app.get("/", (req, res) => {
-    res.send("<h2>Hi There</h2>")
+  res.send("<h2>Hi There</h2>");
 });
 
 const port = process.env.PORT || 4000;
